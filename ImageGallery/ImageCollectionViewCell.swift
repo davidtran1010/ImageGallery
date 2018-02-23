82a5033b42cb6cff8a74a9a10eec718803cf4b15
DavidTran <davidtran@DavidTrans-MacBook-Pro.local>
2018-02-23 15:43:00 +0700

    finished search image from Flickr

---
+++ ImageGallery/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by DavidTran on 2/23/18.
+//  Copyright © 2018 DavidTran. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ImageGallery/ServiceManager.swift
@@ -0,0 +1,9 @@
+//
+//  ServiceManager.swift
+//  ImageGallery
+//
+//  Created by DavidTran on 2/23/18.
+//  Copyright © 2018 DavidTran. All rights reserved.
+//
+
+import Foundation
